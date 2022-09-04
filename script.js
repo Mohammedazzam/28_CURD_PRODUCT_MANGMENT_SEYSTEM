bdc015dd6e994da546bffe8e97301b1b7d4630c5
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2022-09-04 13:55:22 +0300

    عمل عرض للداتا read

--- script.js
+++ script.js
@@ -1,67 +1,92 @@
- let title = document.getElementById('title');
- let price = document.getElementById('price');
- let taxes = document.getElementById('taxes');
- let ads = document.getElementById('ads');
- let discount = document.getElementById('discount');
- let total = document.getElementById('total');
- let count = document.getElementById('count');
- let category = document.getElementById('category');
- let submit = document.getElementById('submit');
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
 
 //  console.log(title, price, taxes, taxes, ads, discount, total, count)
 
- // get total
+// get total
 function getTotal() {
-    if(price.value != '') {
+    if (price.value != '') {
         let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
         total.innerHTML = result;
         total.style.background = '#040'
-    }else{
+    } else {
         total.innerHTML = '';
         total.style.background = '#a00d02';
     }
 }
 
- // create product
- let dataPro;
- if(localStorage.product !=null) {
+// create product
+let dataPro;
+if (localStorage.product != null) {
     dataPro = JSON.parse(localStorage.product);
- }else {
+} else {
     dataPro = []
- }
-    // let dataPro = [];
-    submit.onclick = function(){
-        let newPro = {
-            title: title.value,
-            price: price.value,
-            taxes: taxes.value,
-            ads: ads.value,
-            discount: discount.value,
-            total: total.innerHTML,
-            count: count.value,
-            category:category.value
-        }
-        // console.log(newPro);
-        dataPro.push(newPro);
-        localStorage.setItem('product', JSON.stringify(dataPro))
-    
-        clearData();
+}
+// let dataPro = [];
+submit.onclick = function () {
+    let newPro = {
+        title: title.value,
+        price: price.value,
+        taxes: taxes.value,
+        ads: ads.value,
+        discount: discount.value,
+        total: total.innerHTML,
+        count: count.value,
+        category: category.value
     }
+    // console.log(newPro);
+    // save localStorage
+    dataPro.push(newPro);
+    localStorage.setItem('product', JSON.stringify(dataPro))
+
+    clearData();
+    showData()
+}
 
 
- // save localStorage
- // clear inputs
- function clearData() {
+// clear inputs
+function clearData() {
     title.value = '';
     price.value = '';
     taxes.value = '';
     ads.value = '';
     discount.value = '';
     total.innerHTML = '';
-    count.value = ''; 
+    count.value = '';
     category.value = '';
- }
- // read
+}
+
+// read
+function showData() {
+    let table = '';
+    for (let i = 0; i < dataPro.length; i++) {
+        // table =dataPro[i];
+        // console.log(table);
+        table += `
+            <tr>
+                <td>${i}</td>
+                <td>${dataPro[i].title}</td>
+                <td>${dataPro[i].price}</td>
+                <td>${dataPro[i].taxes}</td>
+                <td>${dataPro[i].ads}</td>
+                <td>${dataPro[i].discount}</td>
+                <td>${dataPro[i].total}</td>
+                <td>${dataPro[i].category}</td>
+                <td><button id="update">update</button></td>
+                <td><button id="delete">delete</button></td>
+            </tr>
+        `
+    }
+    document.getElementById('tbody').innerHTML = table;
+}
+showData()
  // count
  // delete product
  // search
